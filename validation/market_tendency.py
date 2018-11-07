d0f9c8ec55046aaf55c418c40b80537be385dcf7
Stefano Casasso <stefano.casasso@predictivelayer.com>
2018-11-07 15:46:12 +0100

    fix problem in copy&paste

--- validation/market_tendency.py
+++ validation/market_tendency.py
@@ -319,8 +319,8 @@ def compute_perfomances(df):
 
     # Add share
     _df['price_diff_abs'] = _df['price_diff'].abs()
-    df['tot_spread_abs_cum'] = _df['price_diff_abs'].cumsum()
-    df['share_cum'] = (_df['gain_cum'] / _df['tot_spread_abs_cum']).round(3)
+    _df['tot_spread_abs_cum'] = _df['price_diff_abs'].cumsum()
+    _df['share_cum'] = (_df['gain_cum'] / _df['tot_spread_abs_cum']).round(3)
 
     return _df
 
