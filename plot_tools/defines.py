f1d6fda2fdd832944539264887fca874cc49f87c
Stefano Casasso <stefano.casasso@predictivelayer.com>
2018-12-13 14:11:20 +0100

    new colors for plots

--- plot_tools/defines.py
+++ plot_tools/defines.py
@@ -22,5 +22,25 @@
 """
 
 # COLOR_LIST_MPL = ['blue', 'orange', 'green', 'red', 'magenta', 'cyan', 'black']
-COLOR_LIST_MPL = ['#092e34', '#46c3b6', '#ffcc5c', '#949494', '#51d5e8', '#ff6f69', '#eddd6f']
+# COLOR_LIST_MPL = ['#092e34', '#46c3b6', '#ffcc5c', '#949494', '#51d5e8', '#ff6f69', '#eddd6f']
+
+# Taken from https://clrs.cc/
+COLOR_LIST_MPL = [
+    '#111111',  # black
+    '#FF4136',  # red
+    '#B10DC9',  # purple
+    '#0074D9',  # blue
+    '#2ECC40',  # green
+    '#AAAAAA',  # gray
+    '#39CCCC',  # teal
+    '#FF851B',  # orange
+    '#3D9970',  # olive
+    '#FFDC00',  # yellow
+    '#F012BE',  # fuchsia
+    '#7FDBFF',  # aqua
+    '#01FF70',  # lime
+    '#85144b',  # maroon
+    '#DDDDDD'  # silver
+    '#001f3f',  # navy
+]
 COLOR_LIST_PLY = ['#17BECF', '#7F7F7F', '#3CB371', '#F0E68C', '#F08080', '#85144b', '#FF851B']
